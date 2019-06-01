e4e020099c789e0b79ccf4b9fccbb42ea25eb349
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-05-31 21:53:20 -0600

    Add files via upload

---
+++ mainTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+public class mainTest {
+
+    @org.junit.Test
+    public void encontrarCentro() {
+        int centroActual=1000;
+        int centroAnterior=500;
+        assertNotEquals(centroActual,centroAnterior);
+    }
+
+    @org.junit.Test
+    public void encontrarRuta() {
+        int ruta=40;
+        int rutaFinal=60;
+        assertNotEquals(ruta,rutaFinal);
+    }
+
+    @org.junit.Test
+    public void encontrarNombre() {
+    }
+
+    @org.junit.Test
+    public void main() {
+    }
+}
