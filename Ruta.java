f367124a532ff2c5cc57ad41dda28d7fed9c3053
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2019-05-31 21:18:45 -0600

    Add files via upload

---
+++ Ruta.java
@@ -0,0 +1,23 @@
+public class Ruta {
+    private String destino;
+    private String fuente;
+    private Integer distancia;
+    /*Esta clase guarda las rutas*/
+    public Ruta(String destino,String fuente, Integer distancia){
+        this.destino = destino;
+        this.fuente = fuente;
+        this.distancia = distancia;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public String getFuente() {
+        return fuente;
+    }
+
+    public Integer getDistancia() {
+        return distancia;
+    }
+}
